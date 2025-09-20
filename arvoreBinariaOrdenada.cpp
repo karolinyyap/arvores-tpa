6fe268050a9b79693ec66f7b739a2e02eac0d84c
Karoliny <131821537+karolinyyap@users.noreply.github.com>
2025-09-20 14:48:56 -0300

    Versão com Busca

--- arvoreBinariaOrdenada.cpp
+++ arvoreBinariaOrdenada.cpp
@@ -23,6 +23,7 @@ struct Aluno{
 struct Alunos{
     Aluno *raiz;
     int quantidade;
+    int alturaMaxima;
 };
 
 Alunos arvore;
@@ -30,56 +31,7 @@ Alunos arvore;
 void inicializa(){
     arvore.raiz = NULL;
     arvore.quantidade = 0;
-}
-
-void atualizaAltura(Aluno *no){
-    if (no->esq == NULL && no->dir == NULL){
-        no->altura = 0;
-    } else {
-        if (altura(no->esq) > altura(no->dir)){
-            no->altura = altura(no->esq);
-        } else {
-            no->altura = altura(no->dir);
-        }
-    }
-}
-
-Aluno* rotacaoSimplesEsquerda(Aluno *raizAtual) {
-    Aluno *novaRaiz = raizAtual->dir;
-    Aluno *filho = novaRaiz->esq;
-
-    novaRaiz->esq = raizAtual->dir;
-    raizAtual->dir = filho;
-
-    atualizaAltura(novaRaiz);
-    atualizaAltura(raizAtual);
-
-    return novaRaiz;
-}
-
-Aluno* rotacaoSimplesDireita(Aluno *raizAtual) {
-    Aluno *novaRaiz = raizAtual->esq;
-    Aluno *filho = novaRaiz->dir;
-
-    novaRaiz->dir = raizAtual->esq;
-    raizAtual->esq = filho;
-
-    atualizaAltura(novaRaiz);
-    atualizaAltura(raizAtual);
-
-    return novaRaiz;
-}
-
-Aluno* rotacaoDuplaEsqDir(Aluno *raizAtual) {
-    raizAtual->esq = rotacaoSimplesEsquerda(raizAtual);
-
-    return rotacaoSimplesDireita(raizAtual);
-}
-
-Aluno* rotacaoDuplaDirEsq(Aluno *raizAtual) {
-    raizAtual->dir = rotacaoSimplesDireita(raizAtual);
-
-    return rotacaoSimplesEsquerda(raizAtual);
+    arvore.alturaMaxima = -1;
 }
 
 int altura(Aluno *no){
@@ -90,37 +42,99 @@ int altura(Aluno *no){
 	}
 }
 
+void atualizaAltura(Aluno *no){
+    if (no->esq == NULL && no->dir == NULL){
+        no->altura = 0;
+    } else {
+        if (altura(no->esq) > altura(no->dir)){
+            no->altura = altura(no->esq) + 1;
+        } else {
+            no->altura = altura(no->dir) + 1;
+        }
+    }
+}
+
+Aluno* rotacaoSimplesEsquerda(Aluno *raizAtual) {
+    Aluno *novaRaiz = raizAtual->dir;
+    Aluno *filho = novaRaiz->esq;
+
+    novaRaiz->esq = raizAtual;
+    raizAtual->dir = filho;
+
+    atualizaAltura(raizAtual);
+    atualizaAltura(novaRaiz);
+
+    return novaRaiz;
+}
+
+
+Aluno* rotacaoSimplesDireita(Aluno *raizAtual) {
+    Aluno *novaRaiz = raizAtual->esq;
+    Aluno *filho = novaRaiz->dir;
+
+    novaRaiz->dir = raizAtual;
+    raizAtual->esq = filho;
+
+    atualizaAltura(raizAtual);
+    atualizaAltura(novaRaiz);
+
+    return novaRaiz;
+}
+
+
+
+Aluno* rotacaoDuplaEsqDir(Aluno *raizAtual) {
+    raizAtual->esq = rotacaoSimplesEsquerda(raizAtual->esq);
+
+    return rotacaoSimplesDireita(raizAtual);
+}
+
+
+Aluno* rotacaoDuplaDirEsq(Aluno *raizAtual) {
+    raizAtual->dir = rotacaoSimplesDireita(raizAtual->dir);
+
+    return rotacaoSimplesEsquerda(raizAtual);
+}
+
+
+
 Aluno* adicionarAluno(Aluno *no, Aluno *aluno) {
     int ladoInserido; //1 - direita; 0 - esquerda
 
     if(no == NULL){
-        return no = aluno;
+        no = aluno;
+        no->dir = NULL;
+        no->esq = NULL;
         arvore.quantidade++;
-    }
-    if(strcmp(aluno->nome, no->nome)){
-        no->dir = adicionarAluno(no->dir, aluno);
-        ladoInserido = 1;
-    }
-    else{
-        no->esq = adicionarAluno(no->esq, aluno);
-        ladoInserido = 0;
+    } else {
+        if(strcmp(aluno->nome, no->nome) >= 0){
+            no->dir = adicionarAluno(no->dir, aluno);
+            ladoInserido = 1;
+        }
+        else{
+            no->esq = adicionarAluno(no->esq, aluno);
+            ladoInserido = 0;
+        }
     }
 
     atualizaAltura(no);
+    if (no->altura > arvore.alturaMaxima) {
+        arvore.alturaMaxima = no->altura;
+    }
 
     int balanceamento = altura(no->esq) - altura(no->dir);
 
-    int balanceamento2;
-    if (ladoInserido == 1){
+    int balanceamento2 = 0;
+    if (ladoInserido == 1 && no->dir != NULL) {
         balanceamento2 = altura(no->dir->esq) - altura(no->dir->dir);
-    } else {
+    } else if (ladoInserido == 0 && no->esq != NULL) {
         balanceamento2 = altura(no->esq->esq) - altura(no->esq->dir);
     }
 
-    if(balanceamento > 1 && balanceamento2 >= 0) {
+    if(balanceamento > 1 && balanceamento2 > 0) {
         return rotacaoSimplesDireita(no);  
     }
-    if(balanceamento < -1 && balanceamento2 <= 0) {
+    if(balanceamento < -1 && balanceamento2 < 0) {
         return rotacaoSimplesEsquerda(no);            
     }
     if(balanceamento > 1 && balanceamento2 < 0) {
@@ -175,6 +189,62 @@ void lerArquivoCSV(const char* nomeArquivo) {
     fclose(arquivo);
 }
 
+Aluno* busca(Aluno *no, const char *nome){
+
+    if (no == NULL) {
+        return NULL;
+    }
+
+    if (strcmp(nome, no->nome) == 0) {
+        return no;
+    } else if(strcmp(nome, no->nome) > 0){
+        return busca(no->dir, nome);
+    } else {
+        return busca(no->esq, nome);
+    }
+}
+
+void lerArqBusca(const char* nomeArquivo){
+    FILE* arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo %s\n", nomeArquivo);
+        return;
+    }
+
+    char nome[50];
+
+    while (fscanf(arquivo, "%49[^\n]\n", nome) == 1) {
+    Aluno* pos = busca(arvore.raiz, nome);
+    if (pos != NULL){
+        std::cout << "Achei!" << "\n";
+    } else {
+        std::cout << "Não achei :(\n";
+        }
+    }
+}
+
+Aluno* exclusao(Aluno* no, const char* nome){
+    
+}
+
+void lerArqExclusao(const char* nomeArquivo){
+    FILE* arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo %s\n", nomeArquivo);
+        return;
+    }
+
+    char nome[50];
+
+    while (fscanf(arquivo, "%49[^\n]\n", nome) == 1) {
+    Aluno* pos = exclusao(arvore.raiz, nome);
+    if (pos != NULL){
+        std::cout << "Exclui!" << "\n";
+    } else {
+        std::cout << "Não achei para excluir :(\n";
+        }
+    }
+}
 
 int main(){
     inicializa();
@@ -184,8 +254,16 @@ int main(){
     inicio = clock();
     lerArquivoCSV("..\\alunos_completosV2.csv");
     fim = clock();
-    printf("Tempo de leitura: %d milissegundos\n", fim - inicio);
+    printf("Tempo de leitura: %d milissegundos\n", fim - inicio); //10 segundos
+    printf("Total de alunos lidos: %d\n", arvore.quantidade);
     system("pause");
+
+    int inicio2, fim2;
+    inicio2 = clock();
+    lerArqBusca("..\\alunos_busca.txt");
+    fim2 = clock();
+    printf("Tempo de leitura: %d milissegundos\n", fim2 - inicio2); //150 milisegundos
+    printf("Altura máxima da árvore: %d\n", arvore.alturaMaxima); //23
 
     return 0;
 }
